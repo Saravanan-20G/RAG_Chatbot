6f8e9be5705b00ca2ae64ed465845be4a164e0ee
Saravanan G <159242456+Saravanan-20G@users.noreply.github.com>
2026-04-14 11:01:25 +0530

    Update create_users.py

--- create_users.py
+++ create_users.py
@@ -1,11 +1,11 @@
-from db import create_user
+from db import create_user, init_db
 
-# create_user("finance_user", "1234", "finance")
-# create_user("hr_user", "1234", "hr")
-# create_user("marketing_user", "1234", "marketing")
+init_db()
 
-
-create_user("analyst_user", "1234", "analyst")
-create_user("admin_user", "1234", "admin")
+create_user("finance_user", "1234", "finance")
+create_user("hr_user", "12345", "hr")
+create_user("marketing_user", "123456", "marketing")
+create_user("analyst_user", "1234789", "analyst")
+create_user("admin_user", "1234562", "admin")
 
 print("Users created")
